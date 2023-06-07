cc377419893fa29cd12b27fc1085816db0700c80
Leandro Leite <leandroleite.ll25@proton.me>
2023-06-07 09:30:05 -0300

    front

--- processo-pixel/src/App.jsx
+++ processo-pixel/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RiSearchLine } from "react-icons/ri";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
+      <div class=' font-sans text-lg font-semibold text-slate-500'>
         <header>
           <label class='flex justify-center bg-slate-600 text-white'>Busca de gateways por ID</label>
         </header>
-        <main class='w-90 h-screen bg-slate-400'>
+        <main class=' bg-slate-400'>
 
-          <div class="flex justify-center items-center h-screen">
-            <form class="bg-slate-900 p-8 rounded shadow-lg flex flex-col">
-              <label class='text-white mb-5 '> Insira o ID do Gateway</label>
-              <input type="text" class='mb-3' />
-              <button class='text-white bg-zinc-700 rounded-2xl hover:' > buscar</button>
+          <div class="flex justify-center  items-center h-screen ">
+            <form class="bg-slate-900 p-8 rounded shadow-lg flex flex-col items-center">
+              <label class='text-white mb-5 flex justify-center'> Insira o ID do Gateway</label>
+              <input required type="text" placeholder='  .  .  .' class='mb-3 mr-3 rounded-2xl ml-3' />
+              <button class=' text-white max-h-max bg-zinc-700 rounded-2xl hover:bg-slate-800 flex items-center' >  Buscar  <RiSearchLine class='ml-2' /></button>
             </form>
           </div>
 
 
         </main>
-        <footer>
 
-        </footer>
       </div>
     </>
 
